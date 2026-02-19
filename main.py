44ac525baa754ed75fb83adba036ac9cb2ee806f
Bobby Ratliff <ratliff.bobby@gmail.com>
2026-02-19 17:45:49 -0600

    Switch to OllamaEmbedding

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 logging.basicConfig(level=logging.WARNING)
 
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.embeddings.ollama import OllamaEmbedding
 from llama_index.core import Settings
 from llama_index.llms.ollama import Ollama
 
@@ -13,8 +13,13 @@ Settings.llm = Ollama(
     base_url="http://localhost:11434"
 )
 
+Settings.embed_model = OllamaEmbedding(
+    model_name="nomic-embed-text",
+    base_url="http://localhost:11434"
+)
+
 documents = SimpleDirectoryReader("my_docs").load_data()
-index = VectorStoreIndex.from_documents(documents, embed_model=HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5"))
+index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
 response = query_engine.query("What are some skills the author has for IT management?")
 print(response)
