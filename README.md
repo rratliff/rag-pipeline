3c846accb44668fe9597a7e3909ebd4414286c66
Bobby Ratliff <ratliff.bobby@gmail.com>
2026-02-19 18:10:37 -0600

    Add README

---
+++ README.md
@@ -0,0 +1,17 @@
+# RAG Pipeline Demo
+
+Demonstrate loading documents and querying using local models.
+
+## Dependencies
+
+* Homebrew Python
+* Homebrew pipenv
+* In PyCharm Settings, add the pipenv as a Python interpreter
+* Ollama
+
+## Ollama setup
+
+```
+ollama pull nomic-embed-text
+ollama pull llama3
+```
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import logging
+import os.path
+import time
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+    Settings
+)
+from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.llms.ollama import Ollama
 
 logging.basicConfig(level=logging.WARNING)
-
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-from llama_index.embeddings.ollama import OllamaEmbedding
-from llama_index.core import Settings
-from llama_index.llms.ollama import Ollama
 
 Settings.llm = Ollama(
     model="llama3",        # any model you have pulled
@@ -18,9 +24,32 @@ Settings.embed_model = OllamaEmbedding(
     base_url="http://localhost:11434"
 )
 
-documents = SimpleDirectoryReader("my_docs").load_data()
-index = VectorStoreIndex.from_documents(documents)
+start_time = time.time()
+
+PERSIST_DIR = "./storage"
+if not os.path.exists(PERSIST_DIR):
+    print("load the documents and create the index in storage")
+    documents = SimpleDirectoryReader("my_docs").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    print("storage directory found. Loading index from storage")
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
+end_time = time.time()
+duration = end_time - start_time
+print(f"Loading completed in {duration:.2f} seconds.")
+
 query_engine = index.as_query_engine()
-response = query_engine.query("What are some skills the author has for IT management?")
+
+query = "What are some skills the author has for IT management?"
+print(query)
+response = query_engine.query(query)
 print(response)
 
+query = "What are some hard drive issues the author has experienced?"
+print(query)
+response = query_engine.query(query)
+print(response)
